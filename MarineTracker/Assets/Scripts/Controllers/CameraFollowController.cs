0734b540627a8fa845230f30a21edb075aa4dff4
prabinKshrestha <prabin2026@gmail.com>
2023-05-20 14:36:08 -0230

    Camera rotation and Vessel fro API pop up in Screen complete

---
+++ MarineTracker/Assets/Scripts/Controllers/CameraFollowController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:53c53b9cf759d833ab53948f7c6cfb51e43683795c7c95c01e097fed6d883d0c
+size 793
---
+++ MarineTracker/Assets/Scripts/Controllers/GameManagerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:950c0082cc7e865539686f879b53ad2a8e25ce11c320daa232ee46b691018e38
+size 841
